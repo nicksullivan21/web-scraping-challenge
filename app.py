dc7c4e4353508d967bf52fe3f0b5a5e5218ac421
nicksullivan21 <74960495+nicksullivan21@users.noreply.github.com>
2021-03-26 14:13:17 +0800

    Commit

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import scrape_mars
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri='mongodb://localhost:27017/mars_app')
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
@@ -17,7 +17,7 @@ def home():
     mars_data = mongo.db.mars.find_one()
 
     # Return template and data
-    return render_template('index.html', mars_info = mars_data)
+    return render_template("index.html", mars_info = mars_data)
 
 # Route that will trigger the scrape function
 @app.route("/scrape")
